62c1aaadb0c7652873cc063317c60b03e8354388
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2021-10-26 20:58:18 -0400

    Update OneStringSwap.java

--- OneStringSwap.java
+++ OneStringSwap.java
@@ -1,12 +1,6 @@
 1790. Check if One String Swap Can Make Strings Equal
 Easy
 
-267
-
-10
-
-Add to List
-
 Share
 You are given two strings s1 and s2 of equal length. A string swap is an operation 
 where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
