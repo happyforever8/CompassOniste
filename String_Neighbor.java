3796cc1f2c3a888d194874bceb51d19fad03a0f8
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2021-10-27 21:59:42 -0400

    Create String_Neighbor.java

---
+++ String_Neighbor.java
@@ -0,0 +1,2 @@
+一个“book”，内容是按 list of pages 给的，比如：{"mary has a", "little lamb", "little lamb"} 这样的，分别是这本书的前三页。然后给定一个单词，
+让找它的neighbors。比如给"little", 返回 {"a", "lamb", "lamb", "lamb"}. FU是处理标点符号和一些edge cas‍‍‍‍‍‍‍‍‌‍‌‌‍‌‌‌‌‌e。还一个FU忘记了。
