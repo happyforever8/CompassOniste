e58f2dc1a585973a8839c0cd45c309c14e6c20ce
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2021-11-03 11:55:34 -0400

    Update String_encode_decode.java

--- String_encode_decode.java
+++ String_encode_decode.java
@@ -95,11 +95,7 @@ public class HelloWorld{
 
 
 
-
-
-
-
-=========leetcode =Decode==========
+=========leetcode =Decode=======394. Decode String===
   Input: s = "3[a]2[bc]"
   Output: "aaabcbc"
   
@@ -152,3 +148,87 @@ public class HelloWorld{
         return sb.toString();
     }
 }
+
+
+===========271. Encode and Decode Strings =====
+     
+     Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+
+Machine 1 (sender) has the function:
+
+string encode(vector<string> strs) {
+  // ... your code
+  return encoded_string;
+}
+Machine 2 (receiver) has the function:
+vector<string> decode(string s) {
+  //... your code
+  return strs;
+}
+So Machine 1 does:
+
+string encoded_string = encode(strs);
+and Machine 2 does:
+
+vector<string> strs2 = decode(encoded_string);
+strs2 in Machine 2 should be the same as strs in Machine 1.
+
+Implement the encode and decode methods.
+
+You are not allowed to solve the problem using any serialize methods (such as eval).
+
+ 
+
+Example 1:
+
+Input: dummy_input = ["Hello","World"]
+Output: ["Hello","World"]
+Explanation:
+Machine 1:
+Codec encoder = new Codec();
+String msg = encoder.encode(strs);
+Machine 1 ---msg---> Machine 2
+     
+     
+     public class Codec {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        if (strs == null || strs.size() == 0){
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        
+        for (String str : strs){
+            sb.append(str.length()).append("/").append(str);
+        }
+        //sb: 5/Hello5/World
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> result = new ArrayList<>();
+        
+        if (s == null || s.length() == 0){
+            return result;
+        }
+        int index = 0;
+        //indexOf(String str, int fromIndex)
+        
+        while (index < s.length()){
+            int slashIndex = s.indexOf("/", index);
+            
+            int len = Integer.parseInt(s.substring(index, slashIndex));
+            
+            index = slashIndex + len + 1;
+            result.add(s.substring(slashIndex + 1, index));
+            
+        }
+        return result;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
